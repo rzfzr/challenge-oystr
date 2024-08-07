ba1912b6a5182ce54f1ae1202621b1ffa604dd3c
rzfzr <capitao.rafa@gmail.com>
2024-08-07 18:04:27 -0300

    Adding error boundary

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Slot } from 'expo-router'
+import { Text } from 'react-native'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ErrorBoundary from '../ErrorBoundary'
 
 export default function AppLayout() {
     return (
-        <QueryClientProvider client={new QueryClient()}>
-            <Slot />
-        </QueryClientProvider >
+        <ErrorBoundary fallback={<Text>Something went wrong</Text>}>
+            <QueryClientProvider client={new QueryClient()}>
+                <Slot />
+            </QueryClientProvider >
+        </ErrorBoundary>
     )
 }
