4737dee1b802da5b15940e988e96f41ea3003e2c
rzfzr <capitao.rafa@gmail.com>
2024-08-07 18:05:55 -0300

    Displaying list of characters

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,16 @@
-import Button from '@mui/material/Button'
-import { Link } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { useCharacters } from '../marvel'
 
 export default function App() {
+
+    const { data, error, isFetching } = useCharacters()
+    if (error && !isFetching) {
+        throw error
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <Link href="/character/thisisanid" asChild>
-                <Button variant="contained">Hello world</Button>
-            </Link>
-            <StatusBar style="auto" />
+            <Text>{JSON.stringify(data)}</Text>
         </View>
     )
 }
