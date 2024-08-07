2f95759459d6019a6e5d7ad17ee5b2fcb6ba9067
rzfzr <capitao.rafa@gmail.com>
2024-08-07 19:01:03 -0300

    Adding global typing

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export declare global {
+    type Character = {
+        id: number
+        name: string
+        description: string
+        modified: string
+        resourceURI: string
+        series: {}
+        stories: {}
+        thumbnail: {}
+        urls: []
+    }
+}
