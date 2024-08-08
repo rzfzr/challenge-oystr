86cec37a272b42aeb9ab90c45ec2f081f1dd7d3b
rzfzr <capitao.rafa@gmail.com>
2024-08-07 21:08:54 -0300

    Adding expandable info

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -6,10 +6,34 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import CardMedia from '@mui/material/CardMedia'
-import IconButton from '@mui/material/IconButton'
+import IconButton, { IconButtonProps } from '@mui/material/IconButton'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import { Collapse, styled } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props
+    return <IconButton {...other} />
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}))
 
 export default function CharacterCard({ character }: { character: Character }) {
+
+    const [expanded, setExpanded] = React.useState(false)
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+
     return (
         <Box sx={{ minWidth: 275 }}>
             <Card variant="outlined">
@@ -19,27 +43,49 @@ export default function CharacterCard({ character }: { character: Character }) {
                     image={`${character.thumbnail.path}.${character.thumbnail.extension}`}
                     title="green iguana"
                 />
-
                 <CardContent>
                     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Secondary Text Above
+                        {/* Secondary Text Above */}
                     </Typography>
                     <Typography variant="h5" component="div">
                         {character.name}
                     </Typography>
                     <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        Secondary Text Below
+                        {/* Secondary Text Below */}
                     </Typography>
                     <Typography variant="body2">
-                        {character.description}
+
                     </Typography>
                 </CardContent>
-                <CardActions>
+                <CardActions disableSpacing>
                     <IconButton aria-label="add to favorites">
                         <FavoriteIcon />
                     </IconButton>
                     <Button size="small">Details</Button>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
                 </CardActions>
+
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>
+                            Seen in {character.comics.available} comics
+                            <br />
+                            Seen in {character.series.available} series
+                            <br />
+                            Seen in {character.stories.available} stories
+                        </Typography>
+                        <Typography paragraph>
+                            {character.description}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
             </Card>
         </Box>
     )
