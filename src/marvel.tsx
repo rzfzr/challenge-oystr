96cd9406385c1e0e8a5e2a1b4ef359a105c9d178
rzfzr <capitao.rafa@gmail.com>
2024-08-08 10:58:57 -0300

    Extracted common params

--- src/marvel.tsx
+++ src/marvel.tsx
@@ -5,19 +5,23 @@ const publicKey = process.env.EXPO_PUBLIC_MARVEL_PUBLIC_KEY
 const privateKey = process.env.EXPO_PUBLIC_MARVEL_PRIVATE_KEY
 const apiUrl = process.env.EXPO_PUBLIC_MARVEL_API_URL
 
+const getDefaultParams = () => {
+    const timestamp = Date.now()
+    if (!publicKey || !privateKey || !apiUrl) {
+        throw new Error('Missing environment variables for Marvel API')
+    }
+    return new URLSearchParams({
+        ts: String(timestamp),
+        apikey: publicKey,
+        hash: md5(timestamp + privateKey + publicKey),
+        limit: '12'
+    })
+}
+
 export const useCharacters = (searchQuery?: string) => useQuery({
     queryKey: ['characters', searchQuery],
     queryFn: async (): Promise<Character[]> => {
-        const timestamp = Date.now()
-        if (!publicKey || !privateKey || !apiUrl) {
-            throw new Error('Missing environment variables for Marvel API')
-        }
-        const params = new URLSearchParams({
-            ts: String(timestamp),
-            apikey: publicKey,
-            hash: md5(timestamp + privateKey + publicKey),
-            limit: '12'
-        })
+        const params = getDefaultParams()
 
         if (searchQuery) {
             params.append('nameStartsWith', searchQuery)
@@ -39,15 +43,7 @@ export const useCharacters = (searchQuery?: string) => useQuery({
 export const useCharacter = (id: string) => useQuery({
     queryKey: ['character', id],
     queryFn: async (): Promise<Character> => {
-        const timestamp = Date.now()
-        if (!publicKey || !privateKey || !apiUrl) {
-            throw new Error('Missing environment variables for Marvel API')
-        }
-        const params = new URLSearchParams({
-            ts: String(timestamp),
-            apikey: publicKey,
-            hash: md5(timestamp + privateKey + publicKey),
-        })
+        const params = getDefaultParams()
 
         const fetchUrl = `${apiUrl}/characters/${id}?` + params
         const response = await fetch(fetchUrl)
