7a8d1d769dd516e3c32c2f59d7eab9e3b97679c3
rzfzr <capitao.rafa@gmail.com>
2024-08-08 12:08:12 -0300

    Showing cached cached version

--- src/app/character/[id].tsx
+++ src/app/character/[id].tsx
@@ -2,17 +2,27 @@ import { useLocalSearchParams, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
 import { useCharacter } from '../../marvel'
 import CharacterDetails from '../../components/CharacterDetails'
+import { useStore } from '../../useStore'
 
 export default function Character() {
     const { id } = useLocalSearchParams()
-    const { data: character, error, isFetching } = useCharacter(id.toString())
+    const { data, error, isFetching } = useCharacter(id.toString())
+
+    const character = useStore(state => state.getCharacter(id.toString()))
+    const updateCharacters = useStore(state => state.updateCharacters)
+
+    if (data) {
+        updateCharacters([data])
+    }
 
     const navigation = useNavigation()
     useEffect(() => {
         navigation.setOptions({ title: character?.name ? character.name + ' details' : 'Loading' })
     }, [navigation, character?.name])
 
+    console.log('map', character)
     console.log('data single', character, error, isFetching)
 
-    return (character && <CharacterDetails character={character} />)
+    return ((character || data) &&
+        <CharacterDetails character={character || data} />)
 }
