cda2e3c6830738355a97d983febac2b6cca24ca1
rzfzr <capitao.rafa@gmail.com>
2024-08-08 11:53:27 -0300

    Updating state with new data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,26 @@
-import { View } from 'react-native'
 import { useCharacters } from '../marvel'
 import CharactersGrid from '../components/CharactersGrid'
 import SearchBar from '../components/SearchBar'
 import { useEffect, useState } from 'react'
 import { useDebounce } from '../useDebounce'
 import { useNavigation } from 'expo-router'
+import { useStore } from '../useStore'
 
 export default function App() {
     const [searchQuery, setSearchQuery] = useState('')
     const debouncedSearchQuery = useDebounce(searchQuery, 500)
+
+    const addCharacters = useStore(state => state.addCharacters)
 
     const { data: characters, error, isFetching } = useCharacters(debouncedSearchQuery)
     console.log('data', characters, error, isFetching)
 
     if (error && !isFetching) {
         throw error
+    }
+
+    if (characters) {
+        addCharacters(characters)
     }
 
     const navigation = useNavigation()
